193a912d14f1ee7cc298337c0eafb920df3c51d4
Lucas Rocha <lucas.rocha-dev@outlook.com>
2026-01-15 18:31:33 -0300

    Exercicio19

--- Java-Rev-Sonda/src/br/com/rev/Exercicio19.java
+++ Java-Rev-Sonda/src/br/com/rev/Exercicio19.java
@@ -1,4 +1,17 @@
 package br.com.rev;
 
+import java.util.Scanner;
+
 public class Exercicio19 {
+    public static void main(String[] args) {
+
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Insira o valor inteiro entre 1 --- 1000");
+        Integer valor01 = entrada.nextInt(); entrada.nextLine();
+        for(int x = 0; x <= valor01; x++){
+            if(x % 2 == 1){
+                System.out.printf("%d \n", x);
+            }
+        }
+    }
 }
