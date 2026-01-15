57f41adf90dc5baa9d992cb6e4e69495bdcf5d67
Lucas Rocha <lucas.rocha-dev@outlook.com>
2026-01-15 17:52:25 -0300

    Exercicio1

---
+++ Java-Rev-Sonda/src/br/com/rev/Main.java
@@ -0,0 +1,19 @@
+package br.com.rev;
+
+/*@author LUCAS ROCHA */
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner input = new Scanner(System.in);
+        Double valueOne, valueTwo;
+        System.out.println("Enter Value");
+        valueOne = input.nextDouble();
+        valueTwo = input.nextDouble();
+        System.out.printf("Soma %.2f", valueOne + valueTwo);
+        input.close();
+    }
+}
